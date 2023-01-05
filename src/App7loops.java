2ca0ad9b8dd4fc6740c00882dbf7a9b9473673bb
hiteshpakhan <hitesh5pakhan@gmail.com>
2023-01-05 22:37:06 +0530

    a

--- src/App7loops.java
+++ src/App7loops.java
@@ -37,23 +37,72 @@
 
 
 
-// print the sum of first n natural number
-import java.util.*;
+// Question : print the sum of first n natural number
+// import java.util.*;
 
+// public class App7loops{
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+
+//         System.out.print("enter the number : ");
+//         int n = sc.nextInt();
+
+//         int sum = 0;
+        
+//         for(int i = 1; i<=n; i++){
+//             sum = sum + i;
+//         }
+
+//         System.out.println(sum);
+
+//     }
+// }
+
+
+
+
+
+// Question : print the table of a number input by the user
+
+// import java.util.*;
+
+// public class App7loops{
+//     public static void main(String[] args){
+        
+//         Scanner sc = new Scanner(System.in);
+
+//         System.out.print("Enter the number : ");
+//         int n = sc.nextInt();
+
+//         for(int i = 1; i <= 10; i++){
+//             System.out.println(n * i);
+//         }
+
+//     }
+// }
+
+
+
+
+
+
+// make the star pattern
+import java.util.*;
 public class App7loops{
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
+        int n = 4;
+        int m = 5;
 
-        System.out.print("enter the number : ");
-        int n = sc.nextInt();
-
-        int sum = 0;
-        
         for(int i = 1; i<=n; i++){
-            sum = sum + i;
+            
+            // inner loop
+            for(int j = 1; j <= m; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        
         }
 
-        System.out.println(sum);
 
     }
 }
