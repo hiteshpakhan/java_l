7541ff834ae1d17916981e2c9b3ce4999015d242
hiteshpakhan <hitesh5pakhan@gmail.com>
2023-02-03 11:10:50 +0530

    a

--- src/AAAAAimp.java
+++ src/AAAAAimp.java
@@ -2,6 +2,9 @@
     // the thing that looks same from the end also from the front
 
 
+// find the factorial of a number 
+    // example: 5 factorial is equal to 5*4*3*1
+
 public class AAAAAimp {
     public static void main(String[] args){
         
--- src/App11_function.java
+++ src/App11_function.java
@@ -1,17 +1,59 @@
 // to name the function we use the method of camel case to name the function name 
 
-import java.util.*;
 
-public class App11_function {
-    public static void printMyName(String name){
-        System.out.println(name);
-        return;
-    }
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("print your name : ");
-        String name = sc.next();
 
-        printMyName(name);
-    }
-}
+
+// import java.util.*;
+
+// public class App11_function {
+//     public static void printMyName(String name){
+//         System.out.println(name);
+//         return;
+//     }
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.print("print your name : ");
+//         String name = sc.next();
+
+//         printMyName(name);
+//     }
+// }
+
+
+
+
+
+
+
+
+
+// import java.util.*;
+
+// public class App11_function {
+
+//     public static int calculate(int a, int b){
+//         int sum = a + b;
+//         return sum;
+//     }
+
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         System.out.print("enter the value of first no : ");
+//         int a = sc.nextInt();
+//         System.out.print("enter the value of second no : ");
+//         int b = sc.nextInt();
+
+//         int sum = calculate(a, b);
+//         System.out.println(sum);
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
